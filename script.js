a896d8507458f83f8371c6319df46995a99b0f78
ojc1234 <ojc123004@gmail.com>
2020-12-12 02:41:26 +0900

    new

---
+++ script.js
@@ -0,0 +1,13 @@
+   function change(color)
+    {
+        document.documentElement.style.setProperty("--myvar", color);
+        document.documentElement.style.setProperty("--myborder", color);
+
+        
+    }   
+     function myborder(color)
+     {
+         document.documentElement.style.setProperty("--myborder", color);
+        
+     }   
+    
